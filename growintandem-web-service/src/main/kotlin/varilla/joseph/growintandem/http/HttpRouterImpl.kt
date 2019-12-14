d6ceb01848eb9543af5dd9ffedbc948e3960dbd1
Joseph Varilla <jvarilla981@gmail.com>
2019-12-14 12:35:43 -0600

    Adding watering schedule request handler router functionality

--- growintandem-web-service/src/main/kotlin/varilla/joseph/growintandem/http/HttpRouterImpl.kt
+++ growintandem-web-service/src/main/kotlin/varilla/joseph/growintandem/http/HttpRouterImpl.kt
@@ -8,6 +8,7 @@ import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import kotlinx.coroutines.launch
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -18,6 +19,7 @@ import varilla.joseph.growintandem.application.ApplicationService
 import varilla.joseph.growintandem.utils.http.*
 import varilla.joseph.growintandem.utils.models.Plant
 import java.lang.Exception
+import java.time.Instant
 import kotlin.coroutines.CoroutineContext
 
 class HttpRouterImpl(private val vertx : Vertx,
@@ -45,9 +47,11 @@ class HttpRouterImpl(private val vertx : Vertx,
 
 
     // Add Routes
-    router.get("$apiBase1/plant").coroutineHandler(this::getPlantsListHandler)
-    router.get("$apiBase1/plant/:id").coroutineHandler(this::getPlantByIdHandler)
+    router.get("$apiBase1/plants").coroutineHandler(this::getPlantsListHandler)
+    router.get("$apiBase1/plants/:id").coroutineHandler(this::getPlantByIdHandler)
 
+    // Example host:port/api/v1/plants/123/watering-schedule?weeks=12&startdate=2003-11-20T11:11:11Z&allowweekends=false
+    router.get("$apiBase1/plants/:id/watering-schedule").coroutineHandler(this::getPlantWateringSchedule)
     return router
   }
 
@@ -111,6 +115,57 @@ class HttpRouterImpl(private val vertx : Vertx,
   }
 
 
+  override suspend fun getPlantWateringSchedule(event :RoutingContext) {
+
+    var request = event.request()
+    var response = event.response()
+
+    try {
+
+      // TODO: Fix so that missing parameters are accounted for
+
+      // Get the query string parameters
+      val plantId = request.getParam("id").trim().toLowerCase()
+
+      // Get the number of weeks to get the schedule for TODO: default to 1 week if not specified
+      val numWeeksToGetScheduleFor = Integer.parseInt(request.getParam("weeks").trim())
+
+      // Parse the start date but default to today's date
+      val scheduleStartDate = Instant.parse(request.getParam("startdate").trim()) ?: Instant.now()
+
+      // Make alloweekends false by default if not true
+      val scheduleForWeekends =
+        request.getParam("allowweekends").trim().toLowerCase() == "true"
+
+
+      // Get the plants list from the application service
+      //val plants = applicationService.getPlantsList()
+      val receivedParams = jsonObjectOf(
+        "id" to plantId,
+        "weeks" to numWeeksToGetScheduleFor,
+        "startdate" to scheduleStartDate,
+        "allowweekends" to scheduleForWeekends
+      )
+
+      // Serialize it
+      val msg = Json.encodePrettily(receivedParams)
+
+      // Send the message
+      response.sendAsJSONWithStatusCode(msg, 200)
+    } catch (reqErrorException :RequestErrorException) {
+
+      // Send the request error message
+      response.sendAsJSONWithStatusCode(
+        Json.encodePrettily(reqErrorException.toErrorMessageObj()), reqErrorException.statusCode)
+    } catch(throwable :Throwable) {
+      when(throwable) {
+        else -> { // If unknown send 500 error
+          response.sendAsJSONWithStatusCode(
+            Json.encodePrettily(SERVER_ERROR_MESSAGE_OBJECT),
+            SERVER_ERROR_MESSAGE_OBJECT.statusCode) }
+      }
+    }
+  }
 
   // This is needed to make coroutines work. Don't worry about how it works for now.
   private fun Route.coroutineHandler(fn: suspend (RoutingContext) -> Unit) {
